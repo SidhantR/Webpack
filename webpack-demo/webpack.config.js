bf91f01326436576a6040527bca4e453542a3200
SidhantR <sidhantrc02@gmail.com>
2024-06-16 16:31:22 +0530

    add dummy app - add entry and output in webpack

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const port = process.env.PORT || 8081
+const webpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    // mode: 'development',
+    entry: './src/index.js',
+    // entry: {       // multiple entry points
+    //     main: './src/index.js',
+    //     vendor: './src'
+    // },
+    output : {          //Specifies where the bundled output should be placed
+        path: path.join(__dirname, '/dist'),
+        filename: 'main.[fullhash].js'
+    },
+    module: {     //Defines how files should be transformed by loaders before they are added to the bundle
+        rules: [
+            {
+                test:/\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    devServer:{      //Configures the development server options for Webpack.
+        host: 'localhost',
+        port: port,
+        historyApiFallback: true,
+        open: true
+    }
+}
